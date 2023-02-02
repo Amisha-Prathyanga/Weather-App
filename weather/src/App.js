8edaae27b699a1222dc85ece4b3615b009de3f09
Amisha Prathyanga <spbamisha@gmail.com>
2023-02-02 23:48:33 +0530

    card modified

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from "./components/header";
 import AddCity from './components/addCity';
 import Home from './Pages/Home';
 import WeatherDetails from "./components/WeatherDetails";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- weather/src/components/WeatherList.js
+++ weather/src/components/WeatherList.js
@@ -1,38 +1,39 @@
+import "holderjs";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 
 import { Link } from "react-router-dom";
-import cloudBack from "../img/Logo.png"
+import cloudBack from "../img/Logo.png";
+import dashBack from "../img/DashBack.jpg";
+import cloud1 from "../img/cloud1.png";
 const WeatherList = ({ details }) => {
   return (
-    <div className="weather-list">
+    <>
       {details.list.map((detail) => (
-        <div className="detail-preview" key={detail.id}>
-          <Row xs={1} md={2} className="g-4">
-            {Array.from({ length: 4 }).map((_, idx) => (
-              <Col>
-                <Card>
-                  <Card.Img variant="top" src={cloudBack} />
-                  <Card.Body>
-                    <Card.Title>{detail.name}</Card.Title>
-                    <Card.Text>
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-          <Link to={`/${detail.id}`}>
-            <h2>{detail.name}</h2>
-            <h2>{detail.coord.lat}</h2>
-          </Link>
-        </div>
+        <Card className="card-main" style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={cloud1} />
+          <Card.Body>
+            <div className="detail-preview" key={detail.id}>
+              <Link to={`/${detail.id}`}>
+                <Card.Title>{detail.name}</Card.Title>
+                <Card.Text>{detail.coord.lat}</Card.Text>
+              </Link>
+            </div>
+          </Card.Body>
+        </Card>
       ))}
-    </div>
+    </>
+    // <div className="weather-list">
+    //   {details.list.map((detail) => (
+    //     <div className="detail-preview" key={detail.id}>
+
+    //       <Link to={`/${detail.id}`}>
+    //         <h2>{detail.name}</h2>
+    //         <h2>{detail.coord.lat}</h2>
+    //       </Link>
+    //     </div>
+    //   ))}
+    // </div>
     // <div>
     //   <h1>{details.list.name}</h1>
     // </div>
