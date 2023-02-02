7837b853696361a10fb61ad411664024a6ab9a8b
Amisha Prathyanga <spbamisha@gmail.com>
2023-02-03 00:23:44 +0530

    image overlay added

--- weather/src/components/WeatherList.js
+++ weather/src/components/WeatherList.js
@@ -8,35 +8,29 @@ import dashBack from "../img/DashBack.jpg";
 import cloud1 from "../img/cloud1.png";
 const WeatherList = ({ details }) => {
   return (
-    <>
+    <div weather-card>
       {details.list.map((detail) => (
-        <Card className="card-main" style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={cloud1} />
-          <Card.Body>
-            <div className="detail-preview" key={detail.id}>
-              <Link to={`/${detail.id}`}>
-                <Card.Title>{detail.name}</Card.Title>
-                <Card.Text>{detail.coord.lat}</Card.Text>
-              </Link>
-            </div>
-          </Card.Body>
-        </Card>
+        
+          <Card className="card-main" style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={cloud1} />
+            <Link to={`/${detail.id}`}>
+            <Card.ImgOverlay>
+              <Card.Title>
+                {detail.name}, {detail.sys.country}
+              </Card.Title>
+            </Card.ImgOverlay>
+            </Link>
+            <Card.Body>
+              <div className="detail-preview" key={detail.id}>
+                <Card.Text>Pressure : {detail.main.pressure}</Card.Text>
+                <Card.Text>Humudity : {detail.main.humidity}</Card.Text>
+                <Card.Text>Visibility : {detail.visibility}</Card.Text>
+              </div>
+            </Card.Body>
+          </Card>
+       
       ))}
-    </>
-    // <div className="weather-list">
-    //   {details.list.map((detail) => (
-    //     <div className="detail-preview" key={detail.id}>
-
-    //       <Link to={`/${detail.id}`}>
-    //         <h2>{detail.name}</h2>
-    //         <h2>{detail.coord.lat}</h2>
-    //       </Link>
-    //     </div>
-    //   ))}
-    // </div>
-    // <div>
-    //   <h1>{details.list.name}</h1>
-    // </div>
+    </div>
   );
 };
 
