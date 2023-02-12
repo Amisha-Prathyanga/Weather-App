7a489a343688a057d518e94250ae6ec5cc29ab49
Amisha Prathyanga <spbamisha@gmail.com>
2023-02-12 23:01:38 +0530

    caching added

--- weather/src/components/useFetch.js
+++ weather/src/components/useFetch.js
@@ -1,15 +1,76 @@
+// import { useEffect, useState } from "react";
+
+// const useFetch = (url) => {
+//   const [data, setData] = useState(null);
+
+//   const [isPending, setIsPennding] = useState(true);
+
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     //abort fetch when going to pages
+//     const abortCont = new AbortController();
+
+//     fetch(url, { signal: abortCont.signal })
+//       .then((res) => {
+//         if (!res.ok) {
+//           throw Error("Could not fetch data from the source");
+//         }
+//         return res.json();
+//       })
+//       .then((data) => {
+//         setData(data);
+//         setIsPennding(false);
+//         setError(null);
+//       })
+//       .catch((err) => {
+//         if (err.name === "AbortError") {
+//           console.log("fetch aborted");
+//         } else {
+//           setIsPennding(false);
+//           setError(err.message);
+//         }
+//       });
+
+//     return () => abortCont.abort();
+//   }, [url]);
+
+//   return { data, isPending, error };
+// };
+
+// export default useFetch;
+
 import { useEffect, useState } from "react";
+
+const CACHE_EXPIRY_TIME = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
-
-  const [isPending, setIsPennding] = useState(true);
-
+  const [timestamp, setTimestamp] = useState(null);
+  const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const cachedData = localStorage.getItem(url);
+    if (cachedData) {
+      const parsedData = JSON.parse(cachedData);
+      if (
+        parsedData.timestamp &&
+        Date.now() - parsedData.timestamp < CACHE_EXPIRY_TIME
+      ) {
+        // If the data is still fresh, return it from the cache
+        setData(parsedData.data);
+        setTimestamp(parsedData.timestamp);
+        setIsPending(false);
+        return;
+      }
+    }
+
     //abort fetch when going to pages
     const abortCont = new AbortController();
+
+    setIsPending(true);
+    setError(null);
 
     fetch(url, { signal: abortCont.signal })
       .then((res) => {
@@ -20,14 +81,16 @@ const useFetch = (url) => {
       })
       .then((data) => {
         setData(data);
-        setIsPennding(false);
+        setTimestamp(Date.now());
+        localStorage.setItem(url, JSON.stringify({ data, timestamp }));
+        setIsPending(false);
         setError(null);
       })
       .catch((err) => {
         if (err.name === "AbortError") {
           console.log("fetch aborted");
         } else {
-          setIsPennding(false);
+          setIsPending(false);
           setError(err.message);
         }
       });
